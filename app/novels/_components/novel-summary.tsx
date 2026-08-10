6087d666de22a40ba7e398dfd6e0c5727461af57
khaoxkongz <thanapat.makkawal@gmail.com>
2026-08-10 09:54:23 +0700

    ลืมเติมคลาส

--- app/novels/_components/novel-summary.tsx
+++ app/novels/_components/novel-summary.tsx
@@ -3,7 +3,7 @@ import { novelInfo } from "@/features/novel/config"
 export function NovelSummary() {
   return (
     <article className="rounded-lg bg-[oklch(0.499_0.0031_106.51/25%)] px-3 py-4 lg:p-8">
-      <h1 className="text-xl leading-tight font-semibold lg:text-[32px]">
+      <h1 className="font-heading text-xl leading-tight font-semibold lg:text-[32px]">
         {novelInfo.title}
       </h1>
       <p className="relative mt-3">{novelInfo.description}</p>
